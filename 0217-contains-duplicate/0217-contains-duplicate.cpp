5bd54d658660205735b989aea78f4ac68c08cf33
Aaditya Raj <107921735+sahay-aaditya-raj@users.noreply.github.com>
2026-08-06 21:48:28 +0530

    Time: 83 ms (17.1%), Space: 111.5 MB (18.87%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,bool> m;
+        for(int i=0; i<nums.size(); i++){
+            if(m[nums[i]]) return true;
+            m[nums[i]] = true;
+        }
+        return false;
+    }
+};
