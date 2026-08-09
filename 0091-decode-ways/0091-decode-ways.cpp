6665dfaedc7ddec48ac77a6521545874aae4fd87
Aaditya Raj <107921735+sahay-aaditya-raj@users.noreply.github.com>
2026-08-09 22:14:27 +0530

    Time: 0 ms (100%), Space: 8.6 MB (64.52%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -4,19 +4,20 @@ public:
     int numDecodings(string s) {
         int n = s.size();
         if (s[0] == '0') return 0;
-
-        vector<int> dp(n + 1, 0);
-        dp[0] = 1;
-        dp[1] = 1;
-        for (int i = 2; i <= n; i++) {
-            if (s[i - 1] != '0') {
-                dp[i] += dp[i - 1];
+        int prev2 = 1;
+        int prev1 = 1;
+        for (int i = 1; i < n; i++) {
+            int cur = 0;
+            if (s[i] != '0') {
+                cur += prev1;
             }
-            int num = stoi(s.substr(i - 2, 2));
+            int num = stoi(s.substr(i - 1, 2));
             if (num >= 10 && num <= 26) {
-                dp[i] += dp[i - 2];
+                cur += prev2;
             }
+            prev2 = prev1;
+            prev1 = cur;
         }
-        return dp[n];
+        return prev1;
     }
 };
